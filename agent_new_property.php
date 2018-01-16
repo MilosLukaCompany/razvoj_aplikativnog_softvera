050e8ff06b0d845fbae67c9457eb2b9392b2335c
Luka Kovacevic <1lordstark@gmail.com>
2018-01-16 02:03:51 +0100

    agent new property

---
+++ agent_new_property.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Singi Stan | Dodaj nekretninu</title>
+        <meta name="description" content="Singi Stan je agencija koja se bavi prometom nekretnina u Beogradu.">
+        <meta name="author" content="Luka Kovacevic & Milos Ratkovic">
+        <meta name="keyword" content="nekretnine, beograd">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800' rel='stylesheet' type='text/css'>
+
+
+        <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="assets/css/normalize.css">
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/fontello.css">
+        <link href="assets/fonts/icon-7-stroke/css/pe-icon-7-stroke.css" rel="stylesheet">
+        <link href="assets/fonts/icon-7-stroke/css/helper.css" rel="stylesheet">
+        <link href="assets/css/animate.css" rel="stylesheet" media="screen">
+        <link rel="stylesheet" href="assets/css/bootstrap-select.min.css"> 
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/icheck.min_all.css">
+        <link rel="stylesheet" href="assets/css/price-range.css">
+        <link rel="stylesheet" href="assets/css/owl.carousel.css">  
+        <link rel="stylesheet" href="assets/css/owl.theme.css">
+        <link rel="stylesheet" href="assets/css/owl.transitions.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/responsive.css">
+        <link rel="stylesheet" href="assets/css/nas_stil.css">
+        <script src="assets/js/google_maps.js" type="text/javascript"></script>
+    </head>
+    <body>
+
+        <div id="preloader">
+            <div id="status">&nbsp;</div>
+        </div>
+        <!-- Body content -->
+        <nav class="navbar navbar-default navbar-fixed-top  ">
+            <div class="container" id="nav_div">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php" id="big_logo"><img src="assets/img/logo.png" alt="logo"></a>
+                    <a class="navbar-brand" href="index.php" id="small_logo"><img src="assets/img/favicon.png" alt="logo"></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse yamm" id="navigation">
+                    <div class="button navbar-right">
+                        
+                            <button class="navbar-btn nav-button wow fadeInRight" onclick="window.open('php/logout.php', '_self');" data-wow-delay="0.48s">Izloguj se</button>
+                            
+                    </div>
+                    <ul class="main-nav nav navbar-nav navbar-right">
+                        
+                        <li class="wow fadeInDown" data-wow-delay="0.3s"><a class="navbar_link" href="index.php">Početna</a></li>
+                        <li class="wow fadeInDown" data-wow-delay="0.4s"><a class="navbar_link" href="properties.php">Nekretnine</a></li>
+                        <li class="wow fadeInDown" data-wow-delay="0.5s"><a class="navbar_link" href="agent_appointment.php">Termini gledanja</a></li>
+                        <li class="wow fadeInDown dropdown ymm-sw " data-wow-delay="0.1s">
+                            <a href="index.html" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="200">Opcije <b class="caret"></b></a>
+                            <ul class="dropdown-menu navbar-nav">
+                                <li>
+                                    <a class="navbar_link" href="agent_new_property.php">Dodaj nekretninu</a></li>
+                                </li>
+                                <li>
+                                    <a class="navbar_link" href="agent_contract.php">Novi ugovor</a></li>
+                                </li>
+                                
+
+                            </ul>
+                        </li>
+                        <li class="wow fadeInDown" data-wow-delay="0.5s"><a class="navbar_link" href="agent_profile.php"><span class="glyphicon glyphicon-cog" aria-hidden="true"></a></li>                     
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+        <div class="page-head" style="margin-top: 99px;"> 
+            <div class="container">
+                <div class="row">
+                    <div class="page-head-content">
+                        <h1 class="page-title">Početna / Agent / Dodaj nekretninu</h1>               
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- property area -->
+        <div class="content-area recent-property padding-top-40" style="background-color: #F4F4F4;">
+            <div class="container">
+                <div class="row">
+                <div class="box-for overflow">
+                <div class="col-md-6">
+                    
+                        <div class="col-md-12 col-xs-12 register-blocks">
+                            <h2>Nova nekretnina: </h2> 
+                            <form action="" method="post">
+                                <div class="form-group">
+                                    <label for="adresa">Adresa</label>
+                                    <input type="text" class="form-control" id="adresa">
+                                </div>
+                                <div class="form-group">
+                                    <label for="m2">Kvadratura</label>
+                                    <input type="number" class="form-control" id="m2">
+                                </div>
+                                <div class="form-group">
+                                    <label for="grejanje">Vrsta grejanja</label>
+                                    <input type="text" class="form-control" id="grejanje">
+                                </div>
+                                <div class="form-group">
+                                    <label for="sprat">Sprat</label>
+                                    <input type="number" class="form-control" id="sprat">
+                                </div>
+                                <div class="form-group">
+                                    <label for="spratnost">Ukupno spratova</label>
+                                    <input type="number" class="form-control" id="spratnost">
+                                </div>
+                                
+                            </form>
+                        </div>
+                    
+                </div>
+
+                <div class="col-md-6">
+                                            
+                        <div class="col-md-12 col-xs-12 login-blocks">
+                            
+                            <form action="" method="post">
+                                <div class="form-group">
+                                    <label for="opstina">Opština</label>
+                                    <br>
+                                    
+                                    <select name="" id="opstina">
+                                        <option value="volvo">Volvo</option>
+                                        <option value="saab">Saab</option>
+                                        <option value="fiat">Fiat</option>
+                                        <option value="audi">Audi</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="tip">Tip nekretnine</label>
+                                    <br>
+                                    
+                                    <select name="" id="tip">
+                                        <option value="volvo">Volvo</option>
+                                        <option value="saab">Saab</option>
+                                        <option value="fiat">Fiat</option>
+                                        <option value="audi">Audi</option>
+                                    </select>
+                                </div>
+                                <label for="cena">Cena</label> <br>
+                                <div class="input-group">
+                                    <span class="input-group-addon">€</span>
+                                    <input type="number" class="form-control" id="cena">
+                                    <span class="input-group-addon">.00</span>
+                                </div>
+                                
+                                <div class="form-group">
+                                     <label for="radio">Namestenost</label>
+                                     <br>
+                                        <input type="radio" name="namestenost" value="namesten" checked> Namesten<br>
+                                        <input type="radio" name="namestenost" value="polunamesten"> Polunamesten<br>
+                                        <input type="radio" name="namestenost" value="nenamesten"> Nenamesten<br><br>
+                                </div>
+                                <div class="form-group">
+                                    <label for="struktura">Struktura</label>
+                                    <br>
+                                    
+                                    <select name="" id="struktura">
+                                        <option value="volvo">Volvo</option>
+                                        <option value="saab">Saab</option>
+                                        <option value="fiat">Fiat</option>
+                                        <option value="audi">Audi</option>
+                                    </select>
+                                </div>
+                                <br>
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-home" aria-hidden="true"> Unesi</button>
+                                </div>
+                            </form>
+                            
+                        </div>
+                        
+                    
+                </div>
+                    </div>    
+                </div>
+            </div>
+        </div> 
+         <!-- Footer area-->
+        <div class="footer-area">
+
+            <div class=" footer">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-md-4 col-sm-6 wow fadeInRight animated">
+                            <div class="single-footer">
+                                <h4>Singi Stan</h4>
+                                <div class="footer-title-line"></div>
+
+
+                                <p>Garantovano vas dovodimo do željenog doma, u rekordnom vremenu.</p>
+                                <ul class="footer-adress">
+                                    <li><i class="pe-7s-map-marker strong"> </i>11000 Beograd, Danijelova 32</li>
+                                    <li><i class="pe-7s-mail strong"> </i> office@singistan.tk</li>
+                                    <li><i class="pe-7s-call strong"> </i>+381 11 235684</li>
+                                </ul>
+
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6 wow fadeInRight animated">
+                            <div class="single-footer">
+                                <h4>Prečice </h4>
+                                <div class="footer-title-line"></div>
+                                <ul class="footer-menu">
+                                    <li><a href="index.php">Početna</a>  </li>
+                                    <li><a href="properties.php">Nekretnine</a>  </li> 
+                                    <li><a href="contact.php">Kontakt </a></li> 
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 col-sm-6 wow fadeInRight animated">
+                            <div class="single-footer news-letter">
+                                <h4>Pretplati se</h4>
+                                <div class="footer-title-line"></div>
+                                <p>Ukoliko želite da dobijate najnovije nekretnine koje se prodaju na vašu e-mail adresu, prijavite se ispod.</p>
+
+                                <form>
+                                    <div class="input-group">
+                                        <input class="form-control" type="text" placeholder="Vaš e-mail ... ">
+                                        <span class="input-group-btn">
+                                            <button class="btn btn-primary subscribe" type="button"><i class="pe-7s-paper-plane pe-2x"></i></button>
+                                        </span>
+                                    </div>
+                                    <!-- /input-group -->
+                                </form> 
+
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-copy text-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="pull-left">
+                            <span> (C) <b>MilosLukaCompany</b> , All rights reserved 2018  </span> 
+                        </div> 
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
+        <script src="assets/js/modernizr-2.6.2.min.js"></script>
+
+        <script src="assets/js/jquery-1.10.2.min.js"></script> 
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/js/bootstrap-select.min.js"></script>
+        <script src="assets/js/bootstrap-hover-dropdown.js"></script>
+
+        <script src="assets/js/easypiechart.min.js"></script>
+        <script src="assets/js/jquery.easypiechart.min.js"></script>
+
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <script src="assets/js/wow.js"></script>
+
+        <script src="assets/js/icheck.min.js"></script>
+        <script src="assets/js/price-range.js"></script>
+
+        <script src="assets/js/main.js"></script>
+        <script src="assets/js/navbar.js" type="text/javascript"></script>
+        
+    </body>
+</html>
